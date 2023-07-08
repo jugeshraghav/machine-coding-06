bb266ff55e3d03df02e22ea158b6fe123033519f
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2023-07-08 13:50:34 +0530

    initial changes

---
+++ src/contexts/DataContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useReducer } from "react";
+
+const DataContext = createContext({ state: {}, dispatch: () => {} });
+
+//Reducer function and initial state
+const initial_state = {};
+const reducer = (state, action) => {};
+
+//Provider
+const DataProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initial_state);
+  return (
+    <DataContext.Provider value={{ state, dispatch }}></DataContext.Provider>
+  );
+};
+
+export const useData = () => useContext(DataContext);
+export default DataProvider;
